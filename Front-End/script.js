ffe1d09ea5e39e28a5d06e71d89a3c8d65bf2c21
Davi Moratorio <quickpay.brasil@gmail.com>
2026-03-25 17:04:50 -0300

    Atualização no Front-End
    - Conexão do index com a tela de login e tela de cadastro;
    -ínicio da Section HOME

---
+++ Front-End/script.js
@@ -0,0 +1,9 @@
+const corpoLogin = document.querySelector("#login");
+function abrirLogin() {
+    window.location.href = 'teladelogin.html';
+}
+
+const corpoCadastro = document.querySelector("#cadastro");
+function abrirCadastro() {
+    window.location.href = 'teladecadastro.html';
+}
