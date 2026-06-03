8a5c120491bd122bf8a43832ea19a769712d6929
Davi Moratorio <quickpay.brasil@gmail.com>
2026-06-03 16:10:05 -0300

    Delete Front-End/script.js

--- Front-End/script.js
+++
@@ -1,9 +0,0 @@
-const corpoLogin = document.querySelector("#login");
-function abrirLogin() {
-    window.location.href = 'teladelogin.html';
-}
-
-const corpoCadastro = document.querySelector("#cadastro");
-function abrirCadastro() {
-    window.location.href = 'teladecadastro.html';
-}
